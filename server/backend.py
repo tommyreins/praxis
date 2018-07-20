309cb11d715145d0041a8b4e3ca761b8179f07b2
tommy <tommy.reins@gmail.com>
2018-07-20 00:32:39 -0700

    removed unusable get function

--- server/backend.py
+++ server/backend.py
@@ -11,29 +11,8 @@ class TReiner(Resource):
       def login(self):
             conn = sqlite3.connect('praxis.db')
             c = conn.cursor()
-            
-            c.execute("SELECT * FROM TASKS")
-            colnames = c.description
-            col_str = ""
-            for row in colnames:
-                  col_str += (row[0] + " ")
-            col_str = col_str.split(" ")
-            data = ObjDict()
-            i = 0
-            for row in c.fetchall():
-                  parsed_id = 0
-                  id = -1
-                  sub_dict = {}
-                  for elem, col in zip(row, col_str):
-                        if parsed_id is 0:
-                              id = str(elem)
-                              parsed_id = 1
-                        else:
-                              sub_dict[col] = elem
-                  data[id] = sub_dict
-            return data
 
-api.add_resource(TReiner, '/api/')
+api.add_resource(TReiner, '/login/')
 
 if __name__ == '__main__':
       app.run(debug=False, host='0.0.0.0')

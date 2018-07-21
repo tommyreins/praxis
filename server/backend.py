4d46936a5c1ecac40ab4bb78f75383f8b8551cd6
tommy <tommy.reins@gmail.com>
2018-07-21 01:32:14 -0700

    google sign in?

--- server/backend.py
+++ server/backend.py
@@ -7,13 +7,14 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-class TReiner(Resource):
-      def login(self):
-            conn = sqlite3.connect('praxis.db')
-            c = conn.cursor()
+
+class Login(Resource):
+    def GET(self):
+        conn = sqlite3.connect('C:\sqlite\praxis.db')
+        c = conn.cursor()
+
 
 api.add_resource(TReiner, '/login/')
 
 if __name__ == '__main__':
-      app.run(debug=False, host='0.0.0.0')
-
+    app.run(debug=False, host='0.0.0.0')

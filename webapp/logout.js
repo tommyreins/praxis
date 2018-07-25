16f39ecdebf5c1b49325ec87a18014a0de6ff4bc
tommy <tommy.reins@gmail.com>
2018-07-25 00:34:48 -0700

    faster logout time, but for real this time... promise

--- webapp/logout.js
+++ webapp/logout.js
@@ -3,9 +3,8 @@ function signMeOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-        location.href = "index.html";
     });
-
+    document.location.href = "index.html";
 }
 function Init() {
     console.log('Init...');

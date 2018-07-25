d42e632e6f6b29a31c86367d244091ed35164884
tommy <tommy.reins@gmail.com>
2018-07-25 00:23:20 -0700

    i was wondering what would break first...

--- webapp/login.js
+++ webapp/login.js
@@ -4,11 +4,11 @@ function onSuccess(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  window.setTimeout(function(){
+  window.setTimeout(function () {
     // Move to a new location or you can do something else
     location.href = "create.html";
-    }, 5000);
-  
+  }, 5000);
+
 }
 function onFailure(error) {
   console.log(error);
@@ -22,18 +22,5 @@ function renderButton() {
     'theme': 'dark',
     'onsuccess': onSuccess,
     'onfailure': onFailure
-  });
-}
-function Init() {
-  console.log('Init...');
-  gapi.load('auth2', function() {
-    gapi.auth2.init();
-  });
-}
-function signMeOut() {
-  console.log('Signing out...');
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
   });
 }

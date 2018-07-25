23835919ededf681adf03262cff84e49c8a6ce97
tommy <tommy.reins@gmail.com>
2018-07-24 23:59:27 -0700

    is logout possible?

--- webapp/login.js
+++ webapp/login.js
@@ -26,12 +26,14 @@ function renderButton() {
 }
 
 function Init() {
+  console.log('Init...');
   gapi.load('auth2', function() {
     gapi.auth2.init();
   });
 }
 
 function signOut() {
+  console.log('Signing out...');
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');

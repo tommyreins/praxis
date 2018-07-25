146395dccc61a2d56007aea81272fbf8313436f0
tommy <tommy.reins@gmail.com>
2018-07-25 00:31:59 -0700

    faster logout time, but for real this time

--- webapp/logout.js
+++ webapp/logout.js
@@ -3,9 +3,7 @@ function signMeOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-        window.setTimeout(function () {
-            location.href = "index.html";
-        }, 1);
+        location.href = "index.html";
     });
 
 }

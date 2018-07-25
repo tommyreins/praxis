a8d41451cf1ce68df6ccaf545d1ebd97f6e26759
tommy <tommy.reins@gmail.com>
2018-07-25 00:12:30 -0700

    fire rises

--- webapp/login.js
+++ webapp/login.js
@@ -32,7 +32,7 @@ function Init() {
   });
 }
 
-function signOut() {
+function signMeOut() {
   console.log('Signing out...');
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {

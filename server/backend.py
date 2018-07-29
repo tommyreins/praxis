da307c728cedd752d84466ebff01f940d308e8b0
tommy <tommy.reins@gmail.com>
2018-07-29 15:50:56 -0700

    improved server logs

--- server/backend.py
+++ server/backend.py
@@ -19,20 +19,23 @@ class Login(Resource):
             # google auth
             # request = requests.Request()
             login_token = request.form['login_token']
-            print('login token received: ' + login_token)
+            print('login token received')
             id_info = id_token.verify_oauth2_token(
                 login_token, requests.Request(), '592019730220-i4op0q91nquh8hoeccreoui2pvvhjr6d.apps.googleusercontent.com')
-            print('iss received: ' + id_info['iss'])
             if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
                 raise ValueError('Wrong issuer.')
+            print('iss: ' + id_info['iss'])            
+            user_id = id_info['sub']
+            print('sub: ' + user_id)
+            user_email = id_info['email']
+            print('email: ' + user_email)
+            user_name = id_info['name']
+            print('name: ' + user_name)
 
-                user_id = id_info['sub']
-                user_email = id_info['email']
-                user_name = id_info['name']
-                conn = sqlite3.connect('C:\sqlite\praxis.db')
-                c = conn.cursor()
-                c.execute('SELECT * FROM users WHERE user_id = {0} and name = {1} and email = {2} and activated = 1'.format(user_id, user_name, user_email))
-                print(c.fetchone)
+            conn = sqlite3.connect('C:\sqlite\praxis.db')
+            c = conn.cursor()
+            c.execute('SELECT * FROM users WHERE user_id = {0} and name = {1} and email = {2} and activated = 1'.format(user_id, user_name, user_email))
+            print(c.fetchone)
 
         except ValueError:
             msg = "something was wrong with the token"

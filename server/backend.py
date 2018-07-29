287a93c6705c677bf4fa408e6b78b9fa84a57ff5
tommy <tommy.reins@gmail.com>
2018-07-29 15:29:17 -0700

    removed try method

--- server/backend.py
+++ server/backend.py
@@ -15,29 +15,29 @@ class Login(Resource):
         return {'hello': 'world'}
 
     def post(self):
-        try:
-            # google auth
-            # request = requests.Request()
-            login_token = request.form['login_token']
-            print('login token received: ' + login_token)
-            id_info = id_token.verify_oauth2_token(
-                login_token, requests.Request(), '592019730220-i4op0q91nquh8hoeccreoui2pvvhjr6d')
-            print('iss received: ' + id_info['iss'])
-            # if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            #     raise ValueError('Wrong issuer.')
+        # try:
+        # google auth
+        # request = requests.Request()
+        login_token = request.form['login_token']
+        print('login token received: ' + login_token)
+        id_info = id_token.verify_oauth2_token(
+            login_token, requests.Request(), '592019730220-i4op0q91nquh8hoeccreoui2pvvhjr6d')
+        print('iss received: ' + id_info['iss'])
+        # if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        #     raise ValueError('Wrong issuer.')
 
-            user_id = id_info['sub']
-            user_email = id_info['email']
-            user_name = id_info['name']
-            conn = sqlite3.connect('C:\sqlite\praxis.db')
-            c = conn.cursor()
-            c.execute('SELECT * FROM users WHERE user_id = {0} and name = {1} and email = {2} and activated = 1'.format(user_id, user_name, user_email))
-            print(c.fetchone)
+        #     user_id = id_info['sub']
+        #     user_email = id_info['email']
+        #     user_name = id_info['name']
+        #     conn = sqlite3.connect('C:\sqlite\praxis.db')
+        #     c = conn.cursor()
+        #     c.execute('SELECT * FROM users WHERE user_id = {0} and name = {1} and email = {2} and activated = 1'.format(user_id, user_name, user_email))
+        #     print(c.fetchone)
 
-        except ValueError:
-            msg = "something was wrong with the token"
-            print(msg)
-            return {'error' : msg}
+        # except ValueError:
+        #     msg = "something was wrong with the token"
+        #     print(msg)
+        #     return {'error' : msg}
 
 
 api.add_resource(Login, '/api/')

145debbbc319aeb45e3199c857250de62a3983ff
tommy <tommy.reins@gmail.com>
2018-07-29 00:10:10 -0700

    url will work, but RIP https :[

--- webapp/login.js
+++ webapp/login.js
@@ -1,7 +1,7 @@
 function onSuccess(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://tomreins.com:5000/login');
+  xhr.open('POST', 'http://ssh.tomreins.com:5000/login');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function () {
     console.log('Signed in as: ' + xhr.responseText);
@@ -13,7 +13,7 @@ function onSuccess(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  //document.location.href = "create.html";
+  document.location.href = "create.html";
 }
 function onFailure(error) {
   console.log(error);

d98842af75d8f67e6097b7640118e772c4ed1bf7
tommy <tommy.reins@gmail.com>
2018-07-30 23:43:24 -0700

    redirect may be interrupting the rest response

--- webapp/login.js
+++ webapp/login.js
@@ -8,13 +8,12 @@ function onSuccess(googleUser) {
     console.log('Signed in as: ' + xhr.responseText);
   };
   xhr.send('login_token=' + id_token);
-
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  document.location.href = "create.html";
+  // document.location.href = "create.html";
 }
 function onFailure(error) {
   console.log(error);
